83741bf7f7d56841639a2bbe401cf559366cae34
AndreaGul <andreagulino99@gmail.com>
2024-06-12 16:30:52 +0200

    feat: footer

--- src/components/Footer/Footer_c.jsx
+++ src/components/Footer/Footer_c.jsx
@@ -1,6 +1,9 @@
+import footerCss from "./Footer.module.css"
 function Footer(){
     return(
-    <><h2>ciao</h2></>
+    <>  <div className={footerCss.container}>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia vero illum accusamus ipsa ipsam, porro itaque veniam natus eligendi animi, deleniti dolore consequatur corrupti quia numquam repudiandae repellat quisquam maxime.</p>
+        </div></>
 )
 }
 
